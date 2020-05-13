693e3f9b5dfaa947f8fe913e55979e2699c12e41
Sarah Vieira <smarievieira@gmail.com>
2020-05-12 23:35:15 -0400

    Rendered recipes to page

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ const APP_ID = '49d151e7';
 const APP_KEY = '486f3eee1c89b97dbc72958c49f4fe6a';
 let userInput = '';
 const form = document.querySelector('form');
-let recipes = [];
+
+const printRecipes = (array) => {
+    document.getElementById('recipes').innerHTML = array.map((recipe, index, array) => {
+        return `
+                <div class="recipe">
+                    <h2>${recipe.recipe.label}</h2>
+                    <img src=${recipe.recipe.image} alt=${recipe.recipe.label}/>
+                    <p>Calorie count: ${Math.ceil(recipe.recipe.calories)}</p>
+                    <a href=${recipe.recipe.url} target="_blank">Recipe here</a>
+                </div>
+                `
+    })
+}
 
 document.querySelector('input').addEventListener('input', (e) => {
     userInput = e.target.value;
@@ -21,11 +33,13 @@ document.querySelector('form').addEventListener('submit', (e) => {
     .then((data) => {
     // .then() handles a successful API call
         console.log('Success!', data.hits);
+        printRecipes(data.hits);
     })
     
     .catch((error) => {
     // .catch() handles an error
         console.log('Something went wrong.' + error)
     });
+
 })
 
